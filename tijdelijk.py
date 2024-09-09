90049fedc7b3b3744920ca8a45e5f2ac2c2c10c5
Gerben Vermeulen <gerbenvermeulen@hotmail.n>
2024-09-09 20:30:02 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,14 +1,18 @@
-prijzen = mijn_dictionery = {
-    "aardbei" : "3",
-    "vanille" : "4",
-    "chocolade" : "5"
-}
-aanbieding = 4 * 0.8
-reclame_tekst = "vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € 3.2"
-print ("vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €",aanbieding)
-# Bij punt 5 reclame_tekst2 zou volgens de opdracht de output er raar uitzien, maar dat is bij mij niet het geval? 
-reclame_tekst3 = reclame_tekst.swapcase()
-reclame_tekst4 = ["vandaag", "in", "de", "aanbieding:", "vanille-ijs,", "1 liter", "slechts", "€ 3.2"]
-for el in reclame_tekst4:
-    print(el) # Bij punt 9 staat dat nu alle letters nu in hoofdletters staan, maar dat is helemaal niet zo? code is print("reclame_tekst"). lower()
-#Punt 10 kom ik ook niet uit, in de leststof staat iets over if-statement, maar gaat alleen over getallen meer of minder, krijg de link met reclame_tekst4 niet goed.
+#Huiswerkopdracht Les 7
+prijzen = { # 1.2
+    "aardbei": 3, 
+    "vanille": 4,
+    "chocolade" : 5 
+    }
+aanbieding = prijzen["aardbei"] * 0.8 #1.3
+reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €{aanbieding}" #1.4
+reclame_tekst2 = reclame_tekst[:62]  #1.5
+reclame_tekst3 = reclame_tekst2.upper() #1.6
+reclame_tekst4 = reclame_tekst3.split() #1.7
+for el in reclame_tekst4: #1.8
+    print(el.lower()) #1.9
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print (el.lower())
